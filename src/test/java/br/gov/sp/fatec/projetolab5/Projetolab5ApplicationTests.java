ac75030d0d368a7060cf8187e091c4dae67b13fc
Valdiney <valdiney.nascimento@fatec.sp.gov.br>
2022-09-08 14:53:40 -0300

    Atalizando projetolab5

--- src/test/java/br/gov/sp/fatec/projetolab5/Projetolab5ApplicationTests.java
+++ src/test/java/br/gov/sp/fatec/projetolab5/Projetolab5ApplicationTests.java
@@ -1,13 +1,35 @@
 package br.gov.sp.fatec.projetolab5;
 
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.gov.sp.fatec.projetolab5.entity.Usuario;
+import br.gov.sp.fatec.projetolab5.repository.UsuarioRepository;
 
 @SpringBootTest
+@Transactional
+@Rollback
 class Projetolab5ApplicationTests {
+	
+	@Autowired
+	private UsuarioRepository usuarioRepo;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void testaInsercao() {
+		Usuario usuario = new Usuario();
+		usuario.setNome("Valdiney");
+		usuario.setSenha("SenhaF0rte");
+		usuarioRepo.save(usuario);
+		assertNotNull(usuario.getId());
+		
 	}
 
 }
