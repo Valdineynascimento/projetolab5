a603298e1eb93292649a960c7265b44d6a91cfca
Valdineynascimento <valdineyjn@gmail.com>
2022-09-09 00:27:10 +0000

    Mapeando tabela autorizacao

--- src/main/java/br/gov/sp/fatec/projetolab5/entity/Usuario.java
+++ src/main/java/br/gov/sp/fatec/projetolab5/entity/Usuario.java
@@ -2,9 +2,13 @@ package br.gov.sp.fatec.projetolab5.entity;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -22,6 +26,13 @@ public class Usuario {
 	
 	@Column(name = "usr_senha")
 	private String senha;
+
+	@ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "uau_usuario_autorizacao",
+        joinColumns = { @JoinColumn(name = "usr_id")},
+        inverseJoinColumns = { @JoinColumn(name = "aut_id") }
+    )
+	private Set<Autorizacao> autorizacoes;
 	
 	
 	
@@ -48,5 +59,15 @@ public class Usuario {
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
+	public Set<Autorizacao> getAutorizacoes() {
+		return autorizacoes;
+	}
+
+	public void setAutorizacoes(Set<Autorizacao> autorizacoes) {
+		this.autorizacoes = autorizacoes;
+	}
+
+	
 
 }
